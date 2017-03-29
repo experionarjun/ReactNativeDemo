46940a511594c908bae43f17735d9dfbed1106e6
experionarjun <arjun.sreedhar@experionglobal.com>
2017-03-29 11:48:37 +0530

    style in utils

---
+++ src/utils/styles.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  textbox: {
+    height: 40,
+    width: 300,
+  },
+});
